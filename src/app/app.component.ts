da5b240222047c33ee91be724073990bbb4c56b1
jh-code <jh.wphax@gmail.com>
2019-12-06 00:19:07 -0600

    fix lint and add travis ci

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, OnDestroy {
-  
+
   constructor() {
     //
   }
